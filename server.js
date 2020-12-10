ad9107592aaa00496e268f491596749b0f7e8444
viviantoyota <vikimitoyota@gmail.com>
2020-12-10 18:38:48 -0300

    atualizando server

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-const app = require("./src/app")
-const PORT = 5555
+const express = require("express")
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const app = express()
 
-app.listen(PORT, function(){
+dotenv.config();
+const PORT = process.env.PORT || 5555
+
+const index = require("../projetofinal/src/routes/index")
+const estacoes = require("../projetofinal/src/routes/estacoesRoute")
+
+
+mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+
+    })
+
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", index)
+app.use("/estacoes", estacoes)
+
+app.listen(PORT,  () => {
     console.log(`O APP esta rodando na porta ${PORT}`)
 })
 
+module.exports = app
--- src/configs/database.js
+++ src/configs/database.js
@@ -1,25 +1,3 @@
 
-const mongoose = require('mongoose')
-const dotenv  =  require('dotenv')
 
-dotenv.config();
-
-const DB_URI = process.env.DB_URI 
-const PORT = process.env.PORT
-
-const connect = () => {
-    mongoose.connect(DB_URI,
-        {  useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-            
-        })
-    const connection = mongoose.connection
-    connection.on('error', () => console.error("Erro ao se conectar ao MongoDb"))
-    connection.once('open', () => console.log('Estamos conectados ao Mongo'))
-}
-
-console.log('estou apos a connection,')
-module.exports = { connect }
-
+//mongodb+srv://admin_estacoes:HDY5PID2reW1Yknm@consultametro.fbabv.mongodb.net/consulta-metro?retryWrites=true&w=majority
